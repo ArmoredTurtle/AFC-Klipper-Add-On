b0489b82819cff723547ddda33e62a9ea053c417
Gavin Hanson <gavin@Gavins-MacBook-Pro.local>
2024-09-12 13:42:53 +0100

    Added 2nd Chop to hub cut

--- AFC.py
+++ AFC.py
@@ -614,6 +614,12 @@ class afc:
         self.sleepCmd(0.5)
         # Choppy Chop
         self.gcode.run_script_from_command('SET_SERVO SERVO=cut ANGLE=' + str(self.hub_cut_servo_clip_angle))
+        # KitKat Break
+        self.sleepCmd(0.5)
+        # ReChop, To be doubly choppy sure.
+        self.gcode.run_script_from_command('SET_SERVO SERVO=cut ANGLE=' + str(self.hub_cut_servo_prep_angle))
+        self.sleepCmd(0.5)
+        self.gcode.run_script_from_command('SET_SERVO SERVO=cut ANGLE=' + str(self.hub_cut_servo_clip_angle))
         # Longer Snooze
         self.sleepCmd(1)
         # Align bowden tube (reset)
