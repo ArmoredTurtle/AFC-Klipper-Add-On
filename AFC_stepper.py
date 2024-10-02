b3b5e5f75730cfb151b8f6ec3d54271e12a55f9f
MG-longshot <brianburton88@gmail.com>
2024-10-01 19:21:08 -0500

    Update AFC_stepper.py

--- AFC_stepper.py
+++ AFC_stepper.py
@@ -167,7 +167,7 @@ class AFCExtruderStepper:
            value = speed * -1
         else:
             value = speed
-        value /= 900
+        value /= 500
         if value > 1: value = 1
         if assist_active: self.assist(value)
 
