6fb60514a4ae03b45f2e82c97f9306bfcf6e0da5
Branden Cash <203336+ammmze@users.noreply.github.com>
2024-10-03 23:03:20 -0700

    fix: accept float distances when doing LANE_MOVE

--- AFC.py
+++ AFC.py
@@ -139,9 +139,9 @@ class afc:
     cmd_LANE_MOVE_help = "Lane Manual Movements"
     def cmd_LANE_MOVE(self, gcmd):
         lane = gcmd.get('LANE', None)
-        distance = gcmd.get('DISTANCE', 0)
+        distance = gcmd.get_float('DISTANCE', 0)
         CUR_LANE = self.printer.lookup_object('AFC_stepper ' + lane)
-        CUR_LANE.move(int(distance), self.short_moves_speed, self.short_moves_accel)
+        CUR_LANE.move(distance, self.short_moves_speed, self.short_moves_accel)
 
     def respond_info(self, msg):
         """
