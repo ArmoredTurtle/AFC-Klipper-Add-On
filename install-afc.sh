4e8c8259f1fad896ab6c0e14989c52c1ae7dec72
Eric Sears <esears@gmail.com>
2024-10-10 20:58:04 -0400

    Update to link all *.py files appropriately

--- install-afc.sh
+++ install-afc.sh
@@ -6,9 +6,22 @@ set -e
 export LC_ALL=C
 
 KLIPPER_PATH="${HOME}/klipper"
-EXTENSION_LIST="AFC AFC_buffer AFC_stepper AFC_led AFC_assist"
 GITREPO="https://github.com/ArmoredTurtle/AFC-Klipper-Add-On.git"
+CHECK_EXTENSION="AFC"
 AFC_PATH="${HOME}/AFC-Klipper-Add-On"
+MOONRAKER_CONFIG="${HOME}/printer_data/config"
+KLIPPER_SERVICE=klipper
+
+
+MOONRAKER_UPDATE_CONFIG="""
+[update_manager afc-software]
+type: git_repo
+path: ~/AFC-Klipper-Add-On
+origin: https://github.com/ArmoredTurtle/AFC-Klipper-Add-On.git
+managed_services: klipper moonraker
+primary_branch: ${AFC_BRANCH}
+install_script: install-afc.sh
+"""
 
 function clone_repo() {
   local afc_dir_name afc_base_name
@@ -31,100 +44,120 @@ function clone_repo() {
 
 # Step 1:  Verify Klipper has been installed
 function check_klipper() {
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
-        echo "Klipper service found!"
-    else
-        echo "Klipper service not found, please install Klipper first"
-        exit 1
-    fi
+  if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
+    echo "Klipper service found!"
+  else
+    echo "Klipper service not found, please install Klipper first"
+    exit 1
+  fi
 }
 
-# Step 2: Check if the extensions are already present.
+# Step 2: Check if the AFC.py is present.
 # This is a way to check if this is the initial installation.
 function check_existing() {
-    for extension in ${EXTENSION_LIST}; do
-        [ -L "${KLIPPER_PATH}/klippy/extras/${extension}.py" ] || return 1
-    done
-    return 0
+  for extension in ${CHECK_EXTENSION}; do
+    [ -L "${KLIPPER_PATH}/klippy/extras/${extension}.py" ] || return 1
+  done
+  return 0
 }
 
 # Step 3: Link extension to Klipper
 function link_extensions() {
-    echo "Linking extensions to Klipper..."
-    for extension in ${EXTENSION_LIST}; do
-        ln -sf "${AFC_PATH}/${extension}.py" "${KLIPPER_PATH}/klippy/extras/${extension}.py"
-    done
+  echo "Linking extensions to Klipper..."
+  for extension in ${AFC_PATH}/*.py; do
+    # get the basename of the file without the path to link
+    extension=$(basename "$extension")
+    ln -sf "${AFC_PATH}/${extension}" "${KLIPPER_PATH}/klippy/extras/${extension}"
+  done
 }
 
 function unlink_extensions() {
-    echo "Unlinking extensions from Klipper..."
-    for extension in ${EXTENSION_LIST}; do
-        rm -f "${KLIPPER_PATH}/klippy/extras/${extension}.py"
-    done
+  echo "Unlinking extensions from Klipper..."
+  for extension in ${AFC_PATH}/*.py; do
+    # get the basename of the file without the path to link
+    extension=$(basename "$extension")
+    rm -f "${KLIPPER_PATH}/klippy/extras/${extension}"
+  done
 }
 
 # Step 4: Restart Klipper
 function restart_klipper() {
-    echo "Restarting Klipper..."
-    sudo systemctl restart klipper
+  echo "Restarting Klipper..."
+  sudo systemctl restart ${KLIPPER_SERVICE}
 }
 
 function verify_ready() {
-    if [ "$(id -u)" -eq 0 ]; then
-        echo "This script must not run as root"
-        exit 1
+  if [ "$(id -u)" -eq 0 ]; then
+    echo "This script must not run as root"
+    exit 1
+  fi
+}
+
+function restart_moonraker() {
+  echo -e -n "\nRestarting Moonraker...\n"
+  sudo systemctl restart moonraker
+}
+
+function update_moonraker_config() {
+  local moonraker_config
+  if [ $do_uninstall -eq 1 ]; then
+    echo "Ensure you remove the AFC-Klipper-Add-On configuration from Moonraker"
+  else
+    echo -e -n "Updating Moonraker config with AFC-Klipper-Add-On\n"
+    moonraker_config=$(grep -c '\[update_manager afc-software\]' ${MOONRAKER_CONFIG}/moonraker.conf || true)
+    if [ $moonraker_config -eq 0 ]; then
+      echo -e -n "\n${MOONRAKER_UPDATE_CONFIG}" >>${MOONRAKER_CONFIG}/moonraker.conf
+      echo -e -n "Moonraker config updated\n"
+      restart_moonraker
+    else
+      echo -e -n "Moonraker config already configured for AFC-Klipper-Add-On\n"
     fi
+  fi
 }
 
 function show_help() {
-    echo "Usage: install-afc.sh [options]"
-    echo ""
-    echo "Options:"
-    echo "  -k <path>    Specify the path to the Klipper directory"
-    echo "  -u           Uninstall the extensions"
-    echo "  -h           Display this help message"
-    echo ""
-    echo "Example:"
-    echo "  install-afc.sh -k ~/klipper"
-}
-
-function show_moonraker_config() {
-    local REQUIRED_CONFIG="[update_manager afc-software]
-type: git_repo
-path: ~/AFC-Klipper-Add-On
-origin: https://github.com/ArmoredTurtle/AFC-Klipper-Add-On.git
-managed_services: klipper moonraker
-primary_branch: main
-install_script: install-afc.sh
-"
-    echo "Please ensure the following is in your moonraker.conf if you want automatic updates:"
-    echo ""
-    echo "${REQUIRED_CONFIG}"
+  echo "Usage: install-afc.sh [options]"
+  echo ""
+  echo "Options:"
+  echo "  -k <path>                   Specify the path to the Klipper directory"
+  echo "  -c <moonraker config path>  Specify the path to the Moonraker config directory (default: ~/printer_data/config)"
+  echo "  -s <klipper service name>   Specify the name of the Klipper service (default: klipper)"
+  echo "  -u                          Uninstall the extensions"
+  echo "  -h                          Display this help message"
+  echo ""
+  echo "Example:"
+  echo " $0 [-k <klipper_path>] [-s <klipper_service_name>] [-c <moonraker_config_path>] [-u] [-h] "
 }
 
 do_uninstall=0
 
-while getopts "k:uh" arg; do
-    case ${arg} in
-        k) KLIPPER_PATH=${OPTARG} ;;
-        u) do_uninstall=1 ;;
-        h) show_help; exit 0 ;;
-        *) exit 1 ;;
-    esac
+while getopts "k:s:c:uh" arg; do
+  case ${arg} in
+  k) KLIPPER_PATH=${OPTARG} ;;
+  c) MOONRAKER_CONFIG=${OPTARG} ;;
+  s) KLIPPER_SERVICE=${OPTARG} ;;
+  u) do_uninstall=1 ;;
+  h)
+    show_help
+    exit 0
+    ;;
+  *) exit 1 ;;
+  esac
 done
 
 clone_repo
 check_klipper
 verify_ready
 
-if ! check_existing; then
-    link_extensions
-else
-    if [ ${do_uninstall} -eq 1 ]; then
-        unlink_extensions
-    fi
+if [ ${do_uninstall} -eq 1 ]; then
+  echo "Uninstalling AFC-Klipper-Add-On"
+  unlink_extensions
 fi
 
+if ! check_existing && [ $do_uninstall -eq 0 ]; then
+  link_extensions
+fi
+
+update_moonraker_config
 restart_klipper
-show_moonraker_config
 exit 0
