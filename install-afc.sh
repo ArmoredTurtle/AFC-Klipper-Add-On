99a2710bba063d5abd78b83e4ce3f4e1f05bb110
Eric Sears <esears@gmail.com>
2024-10-16 16:33:56 -0400

    More updates on script

--- install-afc.sh
+++ install-afc.sh
@@ -1,14 +1,292 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Force script to exit if an error occurs
 set -e
-
 export LC_ALL=C
 
+# Paths
 KLIPPER_PATH="${HOME}/klipper"
-EXTENSION_LIST="AFC AFC_buffer AFC_stepper AFC_led AFC_assist"
-GITREPO="https://github.com/ArmoredTurtle/AFC-Klipper-Add-On.git"
+MOONRAKER_PATH="${HOME}/printer_data/config"
 AFC_PATH="${HOME}/AFC-Klipper-Add-On"
+PRINTER_CONFIG_PATH="${HOME}/printer_data/config/"
+AFC_CONFIG_PATH="${PRINTER_CONFIG_PATH}/AFC/"
+
+# Variables
+KLIPPER_SERVICE=klipper
+# TODO - Update to real repo
+GITREPO="https://github.com/ejsears/AFC-Klipper-Add-On.git"
+AFC_BRANCH="main"
+
+PRIOR_INSTALLATION=False
+UPDATE_CONFIG=False
+
+# Moonraker Config
+MOONRAKER_UPDATE_CONFIG="""
+[update_manager afc-software]
+type: git_repo
+path: ~/AFC-Klipper-Add-On
+origin: $GITREPO
+managed_services: klipper moonraker
+primary_branch: ${AFC_BRANCH}
+install_script: install-afc.sh
+"""
+
+info_menu() {
+  printf "\e[49m             \e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄\e[49;38;5;143m▀\e[38;5;143;48;5;143m▄\e[38;5;143;49m▄▄\e[49m             \e[m  \n"
+  printf "\e[49m         \e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀\e[38;5;29;48;5;143m▄\e[38;5;29;49m▄\e[38;5;29;48;5;29m▄\e[38;5;29;49m▄\e[38;5;29;48;5;143m▄\e[49;38;5;143m▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄▄\e[49m         \e[m\n"
+  printf "\e[49m      \e[38;5;143;49m▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀\e[38;5;29;48;5;143m▄\e[38;5;29;49m▄\e[38;5;29;48;5;29m▄▄\e[48;5;29m     \e[38;5;29;48;5;29m▄▄\e[38;5;29;49m▄\e[38;5;29;48;5;143m▄\e[49;38;5;143m▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄\e[49m      \e[m\n"
+  printf "\e[49m  \e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀\e[38;5;29;48;5;143m▄\e[38;5;29;49m▄\e[38;5;29;48;5;29m▄\e[48;5;29m   \e[38;5;29;48;5;29m▄▄\e[49;38;5;29m▀▀▀\e[38;5;29;48;5;29m▄▄\e[48;5;29m   \e[38;5;29;48;5;29m▄\e[38;5;29;49m▄\e[38;5;29;48;5;143m▄\e[49;38;5;143m▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄▄\e[49m  \e[m  This installation script will install/update the AFC Klipper extension to your system.\n"
+  printf "\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀\e[38;5;29;48;5;143m▄\e[38;5;29;49m▄\e[38;5;29;48;5;29m▄▄\e[48;5;29m   \e[38;5;29;48;5;29m▄▄\e[49;38;5;29m▀▀\e[49m     \e[49;38;5;29m▀▀\e[38;5;29;48;5;29m▄▄\e[48;5;29m   \e[38;5;29;48;5;29m▄▄\e[38;5;29;49m▄\e[38;5;29;48;5;143m▄\e[49;38;5;143m▀\e[38;5;143;48;5;143m▄▄\e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m     \e[38;5;29;48;5;29m▄\e[49;38;5;29m▀▀\e[49m             \e[49;38;5;29m▀▀\e[38;5;29;48;5;29m▄\e[48;5;29m     \e[49m \e[48;5;143m \e[m  https://discord.gg/armoredturtle\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49;38;5;29m▀\e[38;5;29;49m▄▄\e[38;5;29;48;5;29m▄▄▄▄\e[38;5;29;49m▄▄\e[49m   \e[38;5;29;49m▄▄\e[38;5;29;48;5;29m▄▄▄▄\e[38;5;29;49m▄▄\e[49;38;5;29m▀\e[48;5;29m    \e[49m \e[48;5;143m \e[m  https://github.com/ArmoredTurtle\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m \e[49;38;5;29m▀▀\e[38;5;29;48;5;29m▄\e[48;5;29m \e[38;5;29;48;5;29m▄\e[48;5;29m   \e[38;5;29;48;5;29m▄▄▄\e[48;5;29m   \e[38;5;29;48;5;29m▄\e[48;5;29m \e[38;5;29;48;5;29m▄\e[49;38;5;29m▀▀\e[49m \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m     \e[49;38;5;29m▀▀\e[48;5;29m      \e[38;5;29;48;5;29m▄\e[49;38;5;29m▀▀\e[49m     \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m        \e[48;5;29m     \e[49m        \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m        \e[48;5;29m     \e[49m        \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m        \e[48;5;29m     \e[49m        \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m        \e[48;5;29m     \e[49m        \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[48;5;143m \e[49m \e[48;5;29m    \e[49m        \e[48;5;29m     \e[49m        \e[48;5;29m    \e[49m \e[48;5;143m \e[m\n"
+  printf "\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄\e[38;5;143;48;5;29m▄\e[49;38;5;29m▀\e[38;5;29;48;5;29m▄\e[49m        \e[48;5;29m     \e[49m        \e[38;5;29;48;5;29m▄\e[49;38;5;29m▀\e[38;5;143;48;5;29m▄\e[38;5;143;49m▄\e[38;5;143;48;5;143m▄▄\e[m\n"
+  printf "\e[49m  \e[49;38;5;143m▀▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄▄\e[49m      \e[48;5;29m     \e[49m      \e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀▀\e[49m  \e[m\n"
+  printf "\e[49m      \e[49;38;5;143m▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄▄\e[49m   \e[48;5;29m     \e[49m   \e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀\e[49m      \e[m\n"
+  printf "\e[49m         \e[49;38;5;143m▀▀\e[38;5;143;48;5;143m▄▄\e[38;5;143;49m▄▄\e[49;38;5;29m▀\e[38;5;29;48;5;29m▄\e[49;38;5;29m▀\e[38;5;143;49m▄▄\e[38;5;143;48;5;143m▄▄\e[49;38;5;143m▀▀\e[49m         \e[m\n"
+  printf "\e[49m             \e[49;38;5;143m▀▀\e[38;5;143;48;5;143m▄\e[38;5;143;49m▄\e[38;5;143;48;5;143m▄\e[49;38;5;143m▀▀\e[49m             \e[m\n"
+}
+
+# Colors
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+BOLD_RED='\033[1;31m'
+BOLD_GREEN='\033[1;32m'
+BOLD_YELLOW='\033[1;33m'
+BOLD_BLUE='\033[1;34m'
+BOLD_PURPLE='\033[1;35m'
+BOLD_CYAN='\033[1;36m'
+BOLD_WHITE='\033[1;37m'
+
+# Messages
+INFO="${BLUE}"
+ERROR="${BOLD_RED}"
+SUCCESS="${BOLD_PURPLE}"
+PROMPT="${BOLD_WHITE}"
+WARNING="${BOLD_YELLOW}"
+RESET="${NC}"
+
+print_msg() {
+  local type=$1
+  shift
+  case $type in
+    INFO) echo -e "${INFO}$1${RESET}" ;;
+    WARNING) echo -e "${WARNING}$1${RESET}" ;;
+    ERROR) echo -e "${ERROR}$1${RESET}" ;;
+    PROMPT) echo -e "${PROMPT}$1${RESET}" ;;
+    *) echo -e "$1" ;;
+  esac
+}
+
+check_root() {
+  if [ "$EUID" -eq 0 ]; then
+    print_msg ERROR "Do not run as root, use a normal user"
+    exit 1
+  fi
+}
+
+restart_service() {
+  local service_name=$1
+  print_msh INFO "Restarting ${service_name} service..."
+  if command -v systemctl &> /dev/null; then
+    sudo systemctl restart "${service_name}"
+  else
+    sudo service "${service_name}" restart
+  fi
+}
+
+check_klipper() {
+  if sudo systemctl list-units --full -all -t service --no-legend | grep -q -F "${KLIPPER_SERVICE}.service"; then
+    print_msh SUCCESS "Klipper service found!"
+  else
+    print_msg ERROR "Klipper service not found. Install Klipper first."
+    exit 1
+  fi
+}
+
+check_existing_dirs() {
+  if [ ! -d "${KLIPPER_PATH}" ]; then
+    print_msg ERROR "Klipper directory not found. Use '-k <klipper_dir>' to override."
+    exit 1
+  fi
+
+  if [ ! -d "${MOONRAKER_PATH}" ]; then
+    print_msg ERROR "Moonraker directory not found. Use '-m <moonraker_dir>' to override."
+    exit 1
+  fi
+}
+
+link_extensions() {
+  local extension
+  print_msg INFO "Linking AFC extensions to Klipper..."
+  if [ -d "${KLIPPER_PATH}/klippy/extras" ]; then
+    for extension in "${AFC_PATH}"/extras/*.py; do
+      extension=$(basename "${extension}")
+      ln -sf "${AFC_PATH}/${extension}" "${KLIPPER_PATH}/klippy/extras/${extension}"
+    done
+  else
+    print_msg ERROR "AFC Klipper extensions not installed; Klipper extras directory not found."
+    exit 1
+  fi
+}
+
+unlink_extensions() {
+  local extension
+  print_msg INFO "Unlinking AFC extensions from Klipper..."
+  if [ -d "${KLIPPER_PATH}/klippy/extras" ]; then
+    for extension in "${AFC_PATH}"/extras/*.py; do
+      extension=$(basename "${extension}")
+      rm -f "${KLIPPER_PATH}/klippy/extras/${extension}"
+    done
+  else
+    print_msg ERROR "AFC Klipper extensions not uninstalled; Klipper extras directory not found."
+    exit 1
+  fi
+}
+
+check_existing_install() {
+  local extension
+  print_msg INFO "Checking for prior AFC Klipper installation..."
+  for extension in "${AFC_PATH}"/extras/*.py; do
+    extension=$(basename "${extension}")
+    if [ -L "${KLIPPER_PATH}/klippy/extras/${extension}" ]; then
+      print_msg INFO "Existing installation found..."
+      PRIOR_INSTALLATION=True
+      break
+    else
+      print_msg INFO "No prior installation found..."
+    fi
+  done
+}
+
+backup_afc_config() {
+  if [ -d "${AFC_CONFIG_PATH}" ]; then
+    print_msg INFO "Backing up existing AFC config..."
+    popd "${PRINTER_CONFIG_PATH}"
+    mv "${AFC_CONFIG_PATH}" "${AFC_CONFIG_PATH}.$(date +%y%m%d)"
+    pushd
+  fi
+}
+
+
+prompt_boolean() {
+  local prompt_message=$1
+  local var_name=$2
+  local default_value=$3
+  local input
+
+  echo -ne "${PROMPT}${prompt_message} (True/False) [${PURPLE}Default: ${default_value}${PROMPT}]:${RESET} "
+  read -r input
+  if [ -z "$input" ]; then
+    input=$default_value
+  fi
+
+  if [[ "$input" != "True" && "$input" != "False" ]]; then
+    echo "Invalid input. Please enter 'True' or 'False'."
+    prompt_boolean "$prompt_message" "$var_name" "$default_value"
+  else
+    eval "$var_name=$input"
+  fi
+}
+
+print_section_delimiter() {
+  print_msg SUCCESS "----------------------------------------"
+}
+
+install_type() {
+  echo -ne "
+  ${PROMPT}Please select your installation type:
+  ${CYAN}1) Box Turtle
+  ${PROMPT}Please select an option: ${RESET}"
+  read -r input
+  case $input in
+    1) INSTALLATION_TYPE="BoxTurtle" ;;
+    *) echo "Invalid selection" ;;
+  esac
+}
+
+choose_board_type() {
+  echo -ne "
+  ${PROMPT}Please select your board type:
+  ${CYAN}1) AFC-Lite
+  ${CYAN}2) MMB v1.0
+  ${CYAN}3) MMB v1.1
+  ${PROMPT}Please select an option: ${RESET}"
+  read -r input
+  case $input in
+    1) BOARD_TYPE="AFC-Lite" ;;
+    2) BOARD_TYPE="MMBv1.0" ;;
+    3) BOARD_TYPE="MMBv1.1" ;;
+    *) echo "Invalid selection" ;;
+  esac
+}
+
+toolhead_pin() {
+  echo -ne "
+  ${PROMPT}Please enter your toolhead sensor pin (if known).
+  ${PROMPT}This should be in the format of 'MCU:Pin' (e.g 'EBBCan:PB2').
+  ${ERROR}This is required to be set.
+  ${PROMPT}Press enter if unknown.
+  ${PROMPT}Pin: ${RESET}"
+  read -r input
+  if [ -z "$input" ]; then
+    TOOLHEAD_PIN="UNKNOWN"
+  else
+    TOOLHEAD_PIN=$input
+  fi
+}
+
+buffer_system() {
+  echo -ne "
+  ${PROMPT}Please select your buffer system:
+  ${CYAN}1) TurtleNeck Buffer
+  ${CYAN}2) TurtleNeck v2 Buffer
+  ${CYAN}3) Annex Belay
+  ${CYAN}4) Other
+  ${PROMPT}Please select an option: ${RESET}"
+  read -r input
+  case $input in
+    1) BUFFER_SYSTEM="TurtleNeck" ;;
+    2) BUFFER_SYSTEM="TurtleNeckV2" ;;
+    3) BUFFER_SYSTEM="AnnexBelay" ;;
+    4) BUFFER_SYSTEM="Other" ;;
+    *) echo "Invalid selection" ;;
+  esac
+}
+
+macro_helpers() {
+  local question
+  local var
+  local default
+  print_msg PROMPT "  The following questions will enable / disable various helper macros."
+  print_msg WARNING "  Further configuration for your system is required to be setup in the 'AFC_Macro_Vars.cfg' file."
+
+  declare -A questions=(
+    ["Do you want to enable tip forming?"]="ENABLE_TIP_FORMING False"
+    ["Do you want to enable a toolhead cutter?"]="ENABLE_TOOLHEAD_CUTTER True"
+    ["Do you want to enable the hub cutter?"]="ENABLE_HUB_CUTTER False"
+    ["Do you want to enable the park macro?"]="ENABLE_PARK_MACRO True"
+    ["Do you want to enable the poop macro?"]="ENABLE_POOP_MACRO True"
+    ["Do you want to enable the kick macro?"]="ENABLE_KICK_MACRO True"
+  )
+
+  for question in "${!questions[@]}"; do
+    IFS=' ' read -r var default <<< "${questions[$question]}"
+    prompt_boolean "  $question" "$var" "$default"
+  done
+}
 
 function clone_repo() {
   local afc_dir_name afc_base_name
@@ -29,102 +307,36 @@ function clone_repo() {
   fi
 }
 
-# Step 1:  Verify Klipper has been installed
-function check_klipper() {
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
-        echo "Klipper service found!"
-    else
-        echo "Klipper service not found, please install Klipper first"
-        exit 1
-    fi
-}
-
-# Step 2: Check if the extensions are already present.
-# This is a way to check if this is the initial installation.
-function check_existing() {
-    for extension in ${EXTENSION_LIST}; do
-        [ -L "${KLIPPER_PATH}/klippy/extras/${extension}.py" ] || return 1
-    done
-    return 0
-}
-
-# Step 3: Link extension to Klipper
-function link_extensions() {
-    echo "Linking extensions to Klipper..."
-    for extension in ${EXTENSION_LIST}; do
-        ln -sf "${AFC_PATH}/${extension}.py" "${KLIPPER_PATH}/klippy/extras/${extension}.py"
-    done
-}
-
-function unlink_extensions() {
-    echo "Unlinking extensions from Klipper..."
-    for extension in ${EXTENSION_LIST}; do
-        rm -f "${KLIPPER_PATH}/klippy/extras/${extension}.py"
-    done
-}
-
-# Step 4: Restart Klipper
-function restart_klipper() {
-    echo "Restarting Klipper..."
-    sudo systemctl restart klipper
-}
-
-function verify_ready() {
-    if [ "$(id -u)" -eq 0 ]; then
-        echo "This script must not run as root"
-        exit 1
-    fi
-}
-
-function show_help() {
-    echo "Usage: install-afc.sh [options]"
-    echo ""
-    echo "Options:"
-    echo "  -k <path>    Specify the path to the Klipper directory"
-    echo "  -u           Uninstall the extensions"
-    echo "  -h           Display this help message"
-    echo ""
-    echo "Example:"
-    echo "  install-afc.sh -k ~/klipper"
-}
-
-function show_moonraker_config() {
-    local REQUIRED_CONFIG="[update_manager afc-software]
-type: git_repo
-path: ~/AFC-Klipper-Add-On
-origin: https://github.com/ArmoredTurtle/AFC-Klipper-Add-On.git
-managed_services: klipper moonraker
-primary_branch: main
-install_script: install-afc.sh
-"
-    echo "Please ensure the following is in your moonraker.conf if you want automatic updates:"
-    echo ""
-    echo "${REQUIRED_CONFIG}"
-}
-
-do_uninstall=0
-
-while getopts "k:uh" arg; do
-    case ${arg} in
-        k) KLIPPER_PATH=${OPTARG} ;;
-        u) do_uninstall=1 ;;
-        h) show_help; exit 0 ;;
-        *) exit 1 ;;
-    esac
-done
-
+clear
+check_root
 clone_repo
-check_klipper
-verify_ready
+check_existing_install
+info_menu
 
-if ! check_existing; then
+if [ "$PRIOR_INSTALLATION" = "True" ]; then
+  print_msg WARNING "A prior installation of AFC has been detected."
+  prompt_boolean "Would you like to like to update the config files from the repo?" "update_config_repo" "False"
+  if [ "$update_config_repo" = "True" ]; then
+    backup_afc_config
+    UPDATE_CONFIG=True
+  else
+    print_msg WARNING "Skipping configuration update and updating AFC extensions only."
     link_extensions
-else
-    if [ ${do_uninstall} -eq 1 ]; then
-        unlink_extensions
-    fi
+  fi
 fi
 
-restart_klipper
-show_moonraker_config
-exit 0
+if [ "$PRIOR_INSTALLATION" = "False" ] || [ "$UPDATE_CONFIG" = "True" ]; then
+  install_type
+  choose_board_type
+  toolhead_pin
+  buffer_system
+  print_section_delimiter
+
+  print_msg INFO "  Installation Type: ${INSTALLATION_TYPE}"
+  print_msg INFO "  Board Type: ${BOARD_TYPE}"
+  print_msg INFO "  Toolhead Pin: ${TOOLHEAD_PIN}"
+  print_section_delimiter
+
+  macro_helpers
+fi
+
