60c7a5cd246a4cf3831fd35063f68a07447ff765
MG-longshot <brianburton88@gmail.com>
2024-11-22 20:35:39 -0600

    Update AFC.py

--- extras/AFC.py
+++ extras/AFC.py
@@ -649,10 +649,10 @@ class afc:
                 self.lanes[CUR_LANE.unit][CUR_LANE.name]['spool_id'] = SpoolID
                 self.lanes[CUR_LANE.unit][CUR_LANE.name]['material'] = result['filament']['material']
                 self.lanes[CUR_LANE.unit][CUR_LANE.name]['color'] = '#' + result['filament']['color_hex']
-                if hasattr(result, 'remaining_weight'):
-                    self.lanes[CUR_LANE.unit][CUR_LANE.name]['weight'] =  result['remaining_weight']
-                else:
-                    self.lanes[CUR_LANE.unit][CUR_LANE.name]['weight'] = ''
+                #if hasattr(result, 'remaining_weight'):
+                self.lanes[CUR_LANE.unit][CUR_LANE.name]['weight'] =  result['remaining_weight']
+               # else:
+               #     self.lanes[CUR_LANE.unit][CUR_LANE.name]['weight'] = ''
             else:
                 self.lanes[CUR_LANE.unit][CUR_LANE.name]['spool_id'] = ''
                 self.lanes[CUR_LANE.unit][CUR_LANE.name]['material'] = ''
