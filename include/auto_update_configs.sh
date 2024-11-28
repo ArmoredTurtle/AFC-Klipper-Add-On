6c3d472b8d4a4dacaf10cd88efad8647cdb66bcf
Eric Sears <esears@gmail.com>
2024-11-28 09:45:04 -0500

    Initial commit for version checking

--- include/auto_update_configs.sh
+++ include/auto_update_configs.sh
@@ -11,3 +11,20 @@ function auto_update() {
   # cleanup_blank_lines "${AFC_CONFIG_PATH}/AFC_Hardware-temp.cfg"
   # mv "${AFC_CONFIG_PATH}/AFC_Hardware-temp.cfg" "${AFC_CONFIG_PATH}/AFC_Hardware.cfg"
 }
+
+
+check_version_and_set_force_update() {
+  local version_file=".afc-version"
+  local min_version="1.0.0"
+  local current_version
+
+  if [[ -f "$version_file" ]]; then
+    current_version=$(grep -oP '(?<=AFC_VERSION=)[0-9]+\.[0-9]+\.[0-9]+' "$version_file")
+  fi
+
+  if [[ -z "$current_version" || "$current_version" < "$min_version" ]]; then
+    FORCE_UPDATE=True
+  else
+    FORCE_UPDATE=False
+  fi
+}
