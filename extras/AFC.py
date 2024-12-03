ed24b24374fc2d1f7ae5181ac0dc7a37629f6458
Weemantella <forevernathanj@gmail.com>
2024-12-02 22:42:07 -0700

    add motion queue check

--- extras/AFC.py
+++ extras/AFC.py
@@ -706,6 +706,10 @@ class afc:
         # Activate LED indicator for unloading.
         self.afc_led(self.led_unloading, CUR_LANE.led_index)
 
+        if self.toolhead.motion_queue != CUR_LANE.extruder_name:
+            # Synchronize the extruder stepper with the lane.
+            CUR_LANE.extruder_stepper.sync_to_extruder(CUR_LANE.extruder_name)
+
         # Check and set the extruder temperature if below the minimum.
         wait = True
         pheaters = self.printer.lookup_object('heaters')
