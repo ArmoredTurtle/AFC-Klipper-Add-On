8e7aac57c5a91b2381564b55c830f0b77fd78938
Eric Sears <esears@gmail.com>
2025-04-24 21:31:30 -0400

    Add ability to update AFC macros when using update menu functionality. (#323)
    introduces a new feature to the `update_afc` function in the `include/update_functions.sh` file, allowing users to update AFC-provided macros interactively. The update includes user prompts for confirmation and ensures that custom changes to macros are not overwritten without explicit consent.
    - Added user prompts to confirm whether to update AFC-provided macros, with warnings about overwriting custom changes. This includes macros such as `Brush`, `Cut`, `Kick`, `Park`, `AFC_macros`, and `Poop`.
    - Implemented logic to delete existing macro configuration files and copy updated versions from the AFC configuration directory if the user confirms the update.
    - Updated the `update_message` variable to reflect the success or failure of the macro update process.

--- include/update_functions.sh
+++ include/update_functions.sh
@@ -6,9 +6,32 @@
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
 update_afc() {
+  local macro update_macros confirm_update_macros
+  read -p "Do you want to update the AFC provided macros? (y/n): " update_macros
+  update_macros="${update_macros,,}"
+  if [[ "$update_macros" == "y" ]]; then
+    print_msg WARNING "Updating macros will overwrite any custom changes you have made to these macros."
+    print_msg WARNING "This includes the brush, cut, kick, park, afc_macros, and poop macros."
+    read -p "Please confirm you want to update these macros: (y/n): " confirm_update_macros
+    confirm_update_macros="${confirm_update_macros,,}"
+    if [[ "$confirm_update_macros" == "y" ]]; then
+      for macro in "Brush" "Cut" "Kick" "Park" "Poop" "AFC_macros"; do
+        rm -rf "${afc_config_dir}/macros/${macro}.cfg"
+      done
+      if cp "${afc_path}/config/macros/"*.cfg "${afc_config_dir}/macros/"; then
+        update_message+="""
+AFC Macros updated successfully.
+        """
+      else
+        update_message+="""
+AFC Macros update failed.
+        """
+      fi
+    fi
+  fi
   link_extensions
   remove_t_macros
-  update_message="""
+  update_message+="""
 AFC Klipper Add-On updated successfully.
 """
   export update_message
